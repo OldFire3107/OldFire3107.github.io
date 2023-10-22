acb790279d37212ffefabb16002e21de136dd2d8
Agni Purani <agni.purani@gmail.com>
2023-10-22 18:42:17 +0530

    Timeline update and sitemap

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,7 +1,9 @@
 if (window.innerWidth < 768) {
-	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate data-bss-parallax-bg data-bss-scroll-zoom');
 }
 
 $(document).ready(function(){
-	AOS.init();
+	if ('AOS' in window) {
+		AOS.init();
+	}
 });
